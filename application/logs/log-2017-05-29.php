569356892d9eea1092c222ffdb01578e6ebed251
Hernanda <hernanda17@hotmail.com>
2017-05-29 20:57:55 +0700

    template

---
+++ application/logs/log-2017-05-29.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-05-29 15:54:50 --> Config Class Initialized
+INFO - 2017-05-29 15:54:50 --> Hooks Class Initialized
+DEBUG - 2017-05-29 15:54:50 --> UTF-8 Support Enabled
+INFO - 2017-05-29 15:54:50 --> Utf8 Class Initialized
+INFO - 2017-05-29 15:54:50 --> URI Class Initialized
+DEBUG - 2017-05-29 15:54:50 --> No URI present. Default controller set.
+INFO - 2017-05-29 15:54:50 --> Router Class Initialized
+INFO - 2017-05-29 15:54:50 --> Output Class Initialized
+INFO - 2017-05-29 15:54:50 --> Security Class Initialized
+DEBUG - 2017-05-29 15:54:50 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-05-29 15:54:50 --> Input Class Initialized
+INFO - 2017-05-29 15:54:50 --> Language Class Initialized
+INFO - 2017-05-29 15:54:50 --> Language Class Initialized
+INFO - 2017-05-29 15:54:51 --> Config Class Initialized
+INFO - 2017-05-29 15:54:51 --> Loader Class Initialized
+INFO - 2017-05-29 15:54:51 --> Controller Class Initialized
+DEBUG - 2017-05-29 15:54:51 --> Welcome MX_Controller Initialized
+DEBUG - 2017-05-29 15:54:51 --> File loaded: C:\xampp\htdocs\AutoEmail\application\modules/Welcome/views/welcome_message.php
+INFO - 2017-05-29 15:54:51 --> Final output sent to browser
+DEBUG - 2017-05-29 15:54:51 --> Total execution time: 0.7204
